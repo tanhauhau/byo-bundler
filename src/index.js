2cb54ec5d9892edb556b9e7a0a2f0394192dc4ae
Tan Li Hau <lhtan93@gmail.com>
2019-09-15 14:34:03 +0800

    feat: bundle module

--- src/index.js
+++ src/index.js
@@ -40,6 +40,105 @@ class Module {
       .map(relativePath => resolveRequest(this.filePath, relativePath))
       .map(absolutePath => createModule(absolutePath));
   }
+  transformModuleInterface() {
+    const { types: t } = babel;
+    const { filePath } = this;
+    const { ast, code } = babel.transformFromAstSync(this.ast, this.content, {
+      ast: true,
+      plugins: [
+        function() {
+          return {
+            visitor: {
+              ImportDeclaration(path) {
+                const properties = path.get('specifiers').map(specifier => {
+                  const imported = specifier.isImportDefaultSpecifier()
+                    ? t.identifier('default')
+                    : specifier.get('imported').node;
+                  const local = specifier.get('local').node;
+
+                  return t.objectProperty(imported, local, false, false);
+                });
+                path.replaceWith(
+                  t.variableDeclaration('const', [
+                    t.variableDeclarator(
+                      t.objectPattern(properties),
+                      t.callExpression(t.identifier('require'), [
+                        t.stringLiteral(
+                          resolveRequest(
+                            filePath,
+                            path.get('source.value').node
+                          )
+                        ),
+                      ])
+                    ),
+                  ])
+                );
+              },
+              ExportDefaultDeclaration(path) {
+                path.replaceWith(
+                  t.expressionStatement(
+                    t.assignmentExpression(
+                      '=',
+                      t.memberExpression(
+                        t.identifier('exports'),
+                        t.identifier('default'),
+                        false
+                      ),
+                      t.toExpression(path.get('declaration').node)
+                    )
+                  )
+                );
+              },
+              ExportNamedDeclaration(path) {
+                const declarations = [];
+                if (path.has('declaration')) {
+                  if (path.get('declaration').isFunctionDeclaration()) {
+                    declarations.push({
+                      name: path.get('declaration.id').node,
+                      value: t.toExpression(path.get('declaration').node),
+                    });
+                  } else {
+                    path
+                      .get('declaration.declarations')
+                      .forEach(declaration => {
+                        declarations.push({
+                          name: declaration.get('id').node,
+                          value: declaration.get('init').node,
+                        });
+                      });
+                  }
+                } else {
+                  path.get('specifiers').forEach(specifier => {
+                    declarations.push({
+                      name: specifier.get('exported').node,
+                      value: specifier.get('local').node,
+                    });
+                  });
+                }
+                path.replaceWithMultiple(
+                  declarations.map(decl =>
+                    t.expressionStatement(
+                      t.assignmentExpression(
+                        '=',
+                        t.memberExpression(
+                          t.identifier('exports'),
+                          decl.name,
+                          false
+                        ),
+                        decl.value
+                      )
+                    )
+                  )
+                );
+              },
+            },
+          };
+        },
+      ],
+    });
+    this.ast = ast;
+    this.content = code;
+  }
 }
 
 // resolving
@@ -49,7 +148,71 @@ function resolveRequest(requester, requestPath) {
 
 // bundling
 function bundle(graph) {
-  return [];
+  const modules = collectModules(graph);
+  const moduleMap = toModuleMap(modules);
+  const moduleCode = addRuntime(moduleMap, modules[0].filePath);
+  return [{ name: 'bundle.js', content: moduleCode }];
+}
+
+function collectModules(graph) {
+  const modules = [];
+  collect(graph, modules);
+  return modules;
+
+  function collect(module, modules) {
+    modules.push(module);
+    module.dependencies.forEach(dependency => collect(dependency, modules));
+  }
+}
+
+function toModuleMap(modules) {
+  let moduleMap = '';
+  moduleMap += '{';
+
+  for (const module of modules) {
+    module.transformModuleInterface();
+    moduleMap += `"${module.filePath}": function(exports, require) { ${module.content} },`;
+  }
+
+  moduleMap += '}';
+  return moduleMap;
+}
+
+function addRuntime(moduleMap, entryPoint) {
+  return trim(`
+    const modules = ${moduleMap};
+    const entry = "${entryPoint}";
+    function webpackStart({ modules, entry }) {
+      const moduleCache = {};
+      const require = moduleName => {
+        // if in cache, return the cached version
+        if (moduleCache[moduleName]) {
+          return moduleCache[moduleName];
+        }
+        const exports = {};
+        // this will prevent infinite "require" loop
+        // from circular dependencies
+        moduleCache[moduleName] = exports;
+    
+        // "require"-ing the module,
+        // exported stuff will assigned to "exports"
+        modules[moduleName](exports, require);
+        return moduleCache[moduleName];
+      };
+    
+      // start the program
+      require(entry);
+    }
+
+    webpackStart({ modules, entry });
+    `);
+}
+
+function trim(str) {
+  const lines = str.split('\n').filter(Boolean);
+  const padLength = lines[0].length - lines[0].trimLeft().length;
+  const regex = new RegExp(`^\\s{${padLength}}`);
+  return lines.map(line => line.replace(regex, '')).join('\n');
 }
 
 build({
